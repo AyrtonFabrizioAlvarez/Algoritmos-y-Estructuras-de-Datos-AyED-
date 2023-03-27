fe36f7b2d25d8269e662954d32d9398c3786b270
AyrtonFabrizio <ayrtonfabrizioalvarez@gmail.com>
2023-03-27 17:39:40 -0300

    practica2 AyED

---
+++ Practica2/ListasDeEnteros/src/ej12/ej12.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej12;
+import tp02.ejercicio1.ListaDeEnteros;
+import tp02.ejercicio1.ListaDeEnterosConArreglos;
+/**
+ *
+ * @author Cocalibur
+ */
+public class ej12 {
+    
+}
