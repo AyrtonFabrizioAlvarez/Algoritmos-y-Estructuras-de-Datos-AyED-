7d51e99b25058fcabf5d74bfba3452a09c4ec236
AyrtonFabrizio <93744539+AyrtonFabrizioAlvarez@users.noreply.github.com>
2023-03-30 11:55:28 -0300

    cambios en clase PilaGenerica

--- Practica2/ListasDeEnteros/src/tp02/ejercicio3/PilaGenerica.java
+++ Practica2/ListasDeEnteros/src/tp02/ejercicio3/PilaGenerica.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package tp02.ejercicio3;
-
+import tp02.ejercicio2.*;
 /**
  *
  * @author Coco
  */
-public class PilaGenerica {
+public class PilaGenerica<T> {
+    private ListaEnlazadaGenerica<T> pila;
     
+    public PilaGenerica(){
+        this.pila = new ListaEnlazadaGenerica();
+    }
+    
+    public void apilar(T element){
+        this.pila.agregarFinal(element);
+    }
+    
+    public T desapilar(){
+        if (!this.pila.esVacia()) {
+            T aux = this.pila.elemento(this.pila.tamanio());
+            this.pila.eliminarEn(this.pila.tamanio());
+            return aux;    
+        }
+        else{
+            return null;
+        }
+        
+    }
+    
+    public T tope(){
+        return this.pila.elemento(this.pila.tamanio());
+    }
+    
+    public boolean esVacia(){
+        return this.pila.esVacia();
+    }
 }
