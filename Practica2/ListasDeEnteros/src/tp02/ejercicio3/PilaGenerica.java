953ccc9d92bbd8891146e1ab883e3f5ef9ae1b63
AyrtonFabrizio <93744539+AyrtonFabrizioAlvarez@users.noreply.github.com>
2023-03-30 11:52:43 -0300

    practica2, ej3, creadas clases PilaGenerica y ColaGenerica

---
+++ Practica2/ListasDeEnteros/src/tp02/ejercicio3/PilaGenerica.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp02.ejercicio3;
+
+/**
+ *
+ * @author Coco
+ */
+public class PilaGenerica {
+    
+}
