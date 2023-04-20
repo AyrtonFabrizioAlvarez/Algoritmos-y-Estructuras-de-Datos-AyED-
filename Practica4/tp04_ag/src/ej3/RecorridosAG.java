edd56f61ce7d7c3e7bef7aeba414996ecaf5d426
AyrtonFabrizio <93744539+AyrtonFabrizioAlvarez@users.noreply.github.com>
2023-04-20 11:42:17 -0300

    practica4, ej3

--- Practica4/tp04_ag/src/ej3/RecorridosAG.java
+++ Practica4/tp04_ag/src/ej3/RecorridosAG.java
@@ -16,12 +16,12 @@ public class RecorridosAG {
     public ListaGenerica<Integer> numerosImparesMayoresQuePreOrden (ArbolGeneral<Integer> a, Integer n){
         ListaGenerica<Integer> L = new ListaEnlazadaGenerica<Integer>();
         if (!a.esVacio()) {
-            numerosImpares(L, a, n);
+            numerosImparesPreOrden(L, a, n);
         }
         return L;
     }
     
-    private void numerosImpares(ListaGenerica<Integer> L, ArbolGeneral<Integer> A, Integer n){
+    private void numerosImparesPreOrden(ListaGenerica<Integer> L, ArbolGeneral<Integer> A, Integer n){
         if ( (A.getDato() % 2 == 1) && (A.getDato() > n) ){
             L.agregarFinal(A.getDato());
         }
@@ -29,14 +29,64 @@ public class RecorridosAG {
             ListaGenerica<ArbolGeneral<Integer>> hijos = A.getHijos();
             hijos.comenzar();
             while(!hijos.fin()){
-                numerosImpares(L, hijos.proximo(), n);
+                numerosImparesPreOrden(L, hijos.proximo(), n);
             }
-        }      
+        }
     }
     
     //RECORRIDO
     public ListaGenerica< Integer > numerosImparesMayoresQueInOrden (ArbolGeneral<Integer> a, Integer n){
-        
+        ListaGenerica<Integer> L = new ListaEnlazadaGenerica<Integer>();
+        if (!a.esVacio()) {
+            numerosImparesInOrden(L, a, n);
+        }
+        return L;
+    }
+    
+    private void numerosImparesInOrden(ListaGenerica<Integer> L, ArbolGeneral<Integer> A, Integer n){
+        if (A.tieneHijos()) {
+            ListaGenerica<ArbolGeneral<Integer>> hijos = A.getHijos();
+            
+            //VOY A ANALIZAR EL HIJO MAS IZQUIERDO
+            hijos.comenzar();
+            numerosImparesInOrden(L, hijos.proximo(), n);
+            
+            //VOY A ANALIZAR LA RAIZ
+            if ( (A.getDato() % 2 == 1) && (A.getDato() > n) ) {
+                L.agregarFinal(A.getDato());
+            }
+            
+            //ANALIZO EL RESTO DE LOS HIJOS
+            while (!hijos.fin()) {
+                numerosImparesInOrden(L, hijos.proximo(), n);
+            }
+        }
+        else{
+            if ( (A.getDato() % 2 == 1) && (A.getDato() > n) ) {
+                L.agregarFinal(A.getDato());
+            }
+        }
+    }
+    
+    public ListaGenerica<Integer> numerosImparesMayoresQuePostOrden (ArbolGeneral<Integer> a, Integer n){
+        ListaGenerica<Integer> L = new ListaEnlazadaGenerica();
+        if (!a.esVacio()) {
+            numerosImparesPostOrden(L, a, n);
+        }
+        return L;
+    }
+    
+    private void numerosImparesPostOrden(ListaGenerica<Integer> L, ArbolGeneral<Integer> A, Integer n){
+        if (A.tieneHijos()) {
+            ListaGenerica<ArbolGeneral<Integer>> hijos = A.getHijos();
+            hijos.comenzar();
+            while (!hijos.fin()) {
+                numerosImparesPostOrden(L, hijos.proximo(), n);
+            }
+        }
+        if ( (A.getDato() % 2 == 1) && (A.getDato() > n) ) {
+                L.agregarFinal(A.getDato());
+            }
     }
     
 }
--- Practica4/tp04_ag/src/ej3/pruebaEj3.java
+++ Practica4/tp04_ag/src/ej3/pruebaEj3.java
@@ -16,8 +16,8 @@ public class pruebaEj3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArbolGeneral<Integer> a1 = new ArbolGeneral<Integer>(1);
-        ArbolGeneral<Integer> a2 = new ArbolGeneral<Integer>(5);
+        ArbolGeneral<Integer> a1 = new ArbolGeneral<Integer>(5);
+        ArbolGeneral<Integer> a2 = new ArbolGeneral<Integer>(1);
         ArbolGeneral<Integer> a3 = new ArbolGeneral<Integer>(3);
         ListaGenerica<ArbolGeneral<Integer>> hijos = new ListaEnlazadaGenerica<ArbolGeneral<Integer>>();
         hijos.agregarFinal(a1);
@@ -27,6 +27,12 @@ public class pruebaEj3 {
         
         RecorridosAG recorrido = new RecorridosAG();
         imprimir(recorrido.numerosImparesMayoresQuePreOrden(a, 1));
+        System.out.println("");
+        System.out.println("");
+        imprimir(recorrido.numerosImparesMayoresQueInOrden(a, 1));
+        System.out.println("");
+        System.out.println("");
+        imprimir(recorrido.numerosImparesMayoresQuePostOrden(a, 1));
     }
     
     public static void imprimir(ListaGenerica<Integer> L){
